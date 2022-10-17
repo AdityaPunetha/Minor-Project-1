8f7a261ee9158ece9a13cbfe3fa7cd6c78649278
AdityaPunetha <71438448+AdityaPunetha@users.noreply.github.com>
2022-10-17 11:24:45 +0530

    code

---
+++ main.d
@@ -0,0 +1,4 @@
+obj/main.o: C:\Users\Rajeev\Documents\GitHub\Minor-Project-1/main.c \
+ C:\Users\Rajeev\Documents\GitHub\Minor-Project-1/backprop.h \
+ C:\Users\Rajeev\Documents\GitHub\Minor-Project-1/layer.h \
+ C:\Users\Rajeev\Documents\GitHub\Minor-Project-1/neuron.h
